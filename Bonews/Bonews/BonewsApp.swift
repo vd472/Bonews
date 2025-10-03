79c23914fb7d77476996229b8d5cf47aaed75bcd
vd472 <das.vijayesh@gmail.com>
2025-10-03 13:19:56 +0200

    iOS: Initial project setup for Bonews app

---
+++ Bonews/Bonews/BonewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonewsApp.swift
+//  Bonews
+//
+//  Created by vijayesha on 03.10.25.
+//
+
+import SwiftUI
+
+@main
+struct BonewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bonews/Bonews/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Bonews
+//
+//  Created by vijayesha on 03.10.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    ContentView()
+}
