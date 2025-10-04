49e49de8d3d69c5236a8f2a2d50f4b62413e2fc2
vd472 <das.vijayesh@gmail.com>
2025-10-04 17:45:04 +0200

    feat: create NewsResponse model

---
+++ Bonews/Bonews/Model/NewsResponse.swift
@@ -0,0 +1,33 @@
+//
+//  NewsResponse.swift
+//  Bonews
+//
+//  Created by vijayesha on 04.10.25.
+//
+
+import Foundation
+
+// MARK: - NewsResponse
+struct NewsResponse: Codable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let source: Source
+    let author: String?
+    let title: String
+    let description: String?
+    let url: String
+    let urlToImage: String?
+    let publishedAt: String?
+    let content: String?
+}
+
+// MARK: - Source
+struct Source: Codable {
+    let id: String?
+    let name: String
+}
