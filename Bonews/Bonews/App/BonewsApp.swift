93086984f5c0202ea082b66cbc167976ac3125b9
vd472 <das.vijayesh@gmail.com>
2025-10-06 01:59:44 +0200

    fix: app main to call NewsHeadlinesView instead of ContentView

--- Bonews/Bonews/App/BonewsApp.swift
+++ Bonews/Bonews/App/BonewsApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct BonewsApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                NewsHeadlinesView()
+            }
         }
     }
 }
